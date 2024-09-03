3bce6ae0515f61b5402aec70a43ad349b497ddc8
iristopvt <iristopvt@naver.com>
2024-09-03 12:40:31 +0900

    0501

---
+++ 0501/0501/CBox.cpp
@@ -0,0 +1,19 @@
+//#include "CShape.h"
+//#include <iostream>
+//#include "CBox.h"
+//
+//CBox::CBox()
+//{
+//}
+//
+//CBox::~CBox()
+//{
+//}
+//
+//void CBox::Init(CPoint3 v[8], CPoint _center)
+//{
+//}
+//
+//void CBox::Volum()
+//{
+//}
